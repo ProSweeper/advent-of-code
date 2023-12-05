a5ed335f0fc7f4485f952bfc6041a0949474524c
ProSweeper <dylan.doucet28@gmail.com>
2023-12-05 07:11:55 -0700

    start dayone 2023

---
+++ 2023/DayOne/DayOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using Microsoft.VisualBasic;
+
+var path = "./input.txt";
+
+string numbers = "1234567890";
+var lines = File.ReadAllLines(path);
+int[] values;
+foreach (var line in lines)
+{
+  int firstNumberIndex = -1;
+  int secondNumberIndex = -1;
+
+  for (int i = 0; i < line.Length; i++)
+  {
+    if (numbers.Contains(line[i]))
+    {
+      if (firstNumberIndex < 0)
+      {
+        firstNumberIndex = i;
+      }
+      else
+      {
+        secondNumberIndex = i;
+      }
+    }
+  }
+  if (secondNumberIndex < 0)
+  {
+    secondNumberIndex = firstNumberIndex;
+  }
+  if (firstNumberIndex > -1 && secondNumberIndex > -1)
+  {
+    char firstNum = line[firstNumberIndex];
+    char secondNum = line[secondNumberIndex];
+    string combinedNum = firstNum + secondNum;
+    int combinedNumber = int.Parse(combinedNum);
+    values.Append(combinedNumber);
+  }
+}
