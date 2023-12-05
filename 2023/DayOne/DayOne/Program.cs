c54b548a997f8bf2ac1c55c560225c0aba02665a
Dylan Doucet <ddoucet@local.sequiter.com>
2023-12-05 09:00:35 -0700

    solve day one part one

--- 2023/DayOne/DayOne/Program.cs
+++ 2023/DayOne/DayOne/Program.cs
@@ -3,41 +3,47 @@ using Microsoft.VisualBasic;
 
 var path = "./input.txt";
 
-string numbers = "1234567890";
-var lines = File.ReadAllLines(path);
-int[] values;
-foreach (var line in lines)
-{
-  int firstNumberIndex = -1;
-  int secondNumberIndex = -1;
+string[] test = [
+"1abc2",
+"pqr3stu8vwx",
+"a1b2c3d4e5f",
+"treb7uchet"
+];
+int[] testResults = [12, 38, 15, 77];
 
-  for (int i = 0; i < line.Length; i++)
-  {
-    if (numbers.Contains(line[i]))
-    {
-      if (firstNumberIndex < 0)
-      {
-        firstNumberIndex = i;
-      }
-      else
-      {
-        secondNumberIndex = i;
-      }
-    }
-  }
-  if (secondNumberIndex < 0)
-  {
-    secondNumberIndex = firstNumberIndex;
-  }
-  if (firstNumberIndex > -1 && secondNumberIndex > -1)
-  {
-    char firstNum = line[firstNumberIndex];
-    char secondNum = line[secondNumberIndex];
-    string combinedNum = firstNum + secondNum;
-    int combinedNumber = int.Parse(combinedNum);
-    values.Append(combinedNumber);
-  }
-  foreach (var value in values) {
-	Console.WriteLine
-  }
+string numbers = "1234567890";
+string[] lines = File.ReadAllLines(path);
+int total = 0;
+foreach (string line in lines)
+{
+	int firstNumberIndex = -1;
+	int secondNumberIndex = -1;
+
+	for (int i = 0; i < line.Length; i++)
+	{
+		if (numbers.Contains(line[i]))
+		{
+			if (firstNumberIndex < 0)
+			{
+				firstNumberIndex = i;
+			}
+			else
+			{
+				secondNumberIndex = i;
+			}
+		}
+	}
+	if (secondNumberIndex < 0)
+	{
+		secondNumberIndex = firstNumberIndex;
+	}
+	if (firstNumberIndex > -1 && secondNumberIndex > -1)
+	{
+		char firstNum = line[firstNumberIndex];
+		char secondNum = line[secondNumberIndex];
+		string combinedNum = new string([firstNum, secondNum]);
+		int combinedNumber = int.Parse(combinedNum);
+		total += combinedNumber;
+	}
 }
+Console.WriteLine(total);
