6e0491ee9a1dbee956bf0692adc9344238908a9c
Dylan Doucet <ddoucet@local.sequiter.com>
2023-12-05 10:51:25 -0700

    start part two

---
+++ 2023/DayOnePart2/partTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] example = [
+	"two1nine",
+	"eightwothree",
+	"abcone2threexyz",
+	"xtwone3four",
+	"4nineeightseven2",
+	"zoneight234",
+	"7pqrstsixteen",
+];
+
+string numbers = "1234567890";
+
+static void findNumbers(string input)
+{
+	string[] numberList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",];
+	foreach (string num in numberList)
+	{
+		if (input.Contains(num))
+		{
+			Console.WriteLine(num);
+			Console.WriteLine(Array.IndexOf(numberList, num));
+		}
+	}
+}
+
+
+findNumbers(example[0]);
